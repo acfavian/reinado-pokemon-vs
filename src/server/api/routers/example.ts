de1f931eedf71d80aae278d18105592d7c288317
acfavian <favian.angarita777@gmail.com>
2023-02-19 13:36:01 -0500

    se agrego vista principal de prueba y utilidad para generar id radom para pokemones sin que se repita

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -4,7 +4,9 @@ import { createTRPCRouter, publicProcedure } from "../trpc";
 
 export const exampleRouter = createTRPCRouter({
   hello: publicProcedure
-    .input(z.object({ text: z.string() }))
+    .input(z.object({
+      text: z.string().nullish(),
+    }))
     .query(({ input }) => {
       return {
         greeting: `Hello ${input.text}`,
@@ -13,4 +15,14 @@ export const exampleRouter = createTRPCRouter({
   getAll: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.example.findMany();
   }),
+  // input: z
+  //   .object({
+  //     text:_ z.string().nullish(),
+  //   })
+  //   .nullish(),
+  //   resolve({ input }) {
+  //     return {
+  //       greeting: `hello ${input?.text ?? "world"}`
+  //     }
+  //   }
 });
